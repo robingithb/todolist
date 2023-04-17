4234b09c86604061b0faf6a87444da842592ef31
robingithb <robinthomasq007@gmail.com>
2023-04-18 01:48:46 +0530

    search bar in home page

---
+++ lib/screens/home.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:todo/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: tdBGColor,
+      appBar: _buildAppbar(),
+      body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: Column(
+            children: [
+              searchBox(),
+            ],
+          )),
+    );
+  }
+
+  AppBar _buildAppbar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset("assets/images/avatar.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class searchBox extends StatelessWidget {
+  const searchBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+              Icons.search,
+              color: Colors.black45,
+              size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(maxHeight: 25, minHeight: 20),
+            border: InputBorder.none,
+            hintText: "Search",
+            hintStyle: TextStyle(color: Colors.black45)),
+      ),
+    );
+  }
+}
