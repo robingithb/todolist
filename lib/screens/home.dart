65fa6de021211afc5a7e97f717c4d302c936cef6
robingithb <robinthomasq007@gmail.com>
2023-04-21 14:53:04 +0530

    add new task ui is created

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,33 +17,89 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppbar(),
-      body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-          child: Column(
-            children: [
-              searchBox(),
-              Expanded(
-                child: ListView(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(top: 40, bottom: 20),
-                      child: Text(
-                        "All toDos",
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.w500,
+      body: Stack(children: [
+        Container(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 40, bottom: 20),
+                        child: Text(
+                          "All toDos",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
-                    ),
-                    for (ToDO todoo in todoslist)
-                      ToDoItem(
-                        todo: todoo,
-                      ),
+                      for (ToDO todoo in todoslist)
+                        ToDoItem(
+                          todo: todoo,
+                        ),
+                    ],
+                  ),
+                )
+              ],
+            )),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Row(
+            children: [
+              Expanded(
+                  child: Container(
+                margin: EdgeInsets.only(
+                  bottom: 20,
+                  right: 20,
+                  left: 20,
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(0.0, 0.0),
+                      blurRadius: 10.0,
+                      spreadRadius: 0.0,
+                    )
                   ],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Add new task",
+                    border: InputBorder.none,
+                  ),
+                ),
+              )),
+              Container(
+                margin: EdgeInsets.only(
+                  bottom: 20,
+                  right: 20,
+                ),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    '+',
+                    style: TextStyle(
+                      fontSize: 40,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: tdBlue,
+                    minimumSize: Size(60, 60),
+                    elevation: 10,
+                  ),
                 ),
               )
             ],
-          )),
+          ),
+        )
+      ]),
     );
   }
 
